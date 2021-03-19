af266dda31ed4088618a448f07a44fc3c791ed92
Kenneth Dorian <kenneth.dorian@wustl.edu>
2021-03-19 01:11:03 -0400

    Day2

---
+++ Day2/Day2.py
@@ -0,0 +1,27 @@
+text = open("/Users/kennydorian/Desktop/Indy/AdventOfCode/Day2/Day2.txt", "r")
+list = text.readlines()
+passwords = []
+for row in list:
+    row = row.split(' ')
+    rowDict = {}
+    rowDict.update(min = int(row[0].split('-')[0]), max = int(row[0].split('-')[1]))
+    rowDict.update(letter = row[1].replace(':', ''))
+    rowDict.update(string = row[2].replace('\n', ''))
+    passwords.append(rowDict)
+
+def part1():
+    tot = 0
+    for p in passwords:
+        if p['string'].count(p['letter']) >= p['min'] and p['string'].count(p['letter']) <= p['max']:
+            tot += 1
+    return tot
+# print(part1())
+
+def part2():
+    tot = 0
+    for p in passwords[0:5]:
+        if (p['string'][p['min']-1] == p['letter']) ^ (p['string'][p['max']-1] == p['letter']):
+            print(p)
+            tot += 1
+    return tot
+print(part2())
